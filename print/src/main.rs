63ce706f228610e97124e6138f51f84415ab0899
Toymachine <488434442@qq.com>
2023-06-05 23:30:29 +0800

    format

--- print/src/main.rs
+++ print/src/main.rs
@@ -2,6 +2,7 @@
 /// format!将格式化文本写到字符串   
 /// print!,将文本输出到控制台   
 /// fmt::Display需要通过手动实现
+/// write!会将格式化后的字符串写入缓冲区f中
 fn main() {
 
     println!("Hello, world!");
@@ -31,6 +32,10 @@ fn main() {
     debug();
 
     display();
+
+    list();
+
+    format();
 }
 
 ///调试
@@ -105,4 +110,58 @@ fn display() {
     println!("Compare points:");
     println!("Display: {}", point);
     println!("Debug: {:?}", point);
+}
+
+
+struct List(Vec<i32>);
+
+//为具有多个元素的类型实现fmt::Display
+impl fmt::Display for List {
+
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        //使用元组的下标获取值， 并创建一个vec的引用
+        let vec = &self.0;
+        write!(f, "[")?;
+
+        //迭代vec，并记录迭代次数
+        for (count, v) in vec.iter().enumerate() {
+            //对每一个元素加上逗号，第一个元素除外
+            if count != 0 {
+                write!(f, ", {}: ",count)?;
+            }
+            write!(f, "{}: {}",count, v)?;
+        }
+
+        write!(f, "]")
+    }
+}
+
+fn list() {
+   let v = List(vec![1,2,3,4,5,6,7,8,9,0]);
+    println!("Display: {}", v);
+}
+
+
+#[derive(Debug)]
+struct Color { 
+    red: u8,
+    green: u8,
+    blue: u8,
+}
+
+impl fmt::Display for Color {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "RGB ({0}, {1}, {2}) {:#04x}{:02X}{:02X}", self.red, self.green, self.blue)
+    }
+}
+
+fn format() {
+    
+    for color in [
+        Color {red: 128, green: 255, blue:90},
+        Color {red: 0, green: 3, blue: 254},
+        Color {red: 0, green: 0, blue: 0},
+    ].iter() {
+        println!("{}", *color);
+    }
 }
