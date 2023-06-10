e5d9d0d11086be06cc91bf907622113bad40bf46
Toymachine <488434442@qq.com>
2023-06-10 15:53:16 +0800

    const, static, list

--- custom/src/main.rs
+++ custom/src/main.rs
@@ -4,6 +4,8 @@
 ///元组结构体：具名元组； C 结构体； 单元结构体，常用于泛型中       
 ///枚举
 ///允许创建一个从数个不同取值中选其一的枚举类型
+///常量
+///const and static; const 不可改变， static 具有'static 声明周期， 是可变的变量
 fn main() {
     
     println!("Hello, world!");
@@ -11,6 +13,12 @@ fn main() {
     structs();
 
     use_inspect();
+
+    like_c();
+
+    use_list();
+
+    use_const();
 }
 
 #[derive(Debug)]
@@ -167,3 +175,112 @@ fn use_inspect() {
 }
 
 
+//拥有隐式辨别值
+enum Number {
+    Zero,
+    One,
+    Two,
+}
+
+//拥有显式便别值
+enum Color {
+    Red = 0xff0000,
+    Green = 0x00ff00,
+    Blue = 0x0000ff,
+}
+
+///c语言风格enum 
+fn like_c() {
+
+    //使用use 关键字声明枚举
+    use Number::*;
+    use Color::*;
+    
+    //枚举值转化为整型
+    println!("zero is {}", Zero as i32);
+    println!("One is {}", One as i32);
+    println!("Two is {}", Two as i32);
+
+    println!("roses are #{:06x}", Red as i32);
+    println!("violets are #{:06x}", Blue as i32);
+    println!("leaf are #{:06x}", Green as i32);
+}
+
+///enum 实现链表解构
+enum List {
+    // 元组结构体， 包含一个当前元素和指向下一个节点的指针
+    Cons(u32, Box<List>),
+    //末节点
+    Nil,
+}
+
+use std::fmt::format;
+
+use List::*;
+
+//为List定义方法
+impl List {
+    //创建一个空的链表
+    fn new() -> List {
+        Nil
+    }
+
+    //处理一个链表，在头部插入元素并返回处理后的链表
+    fn prepend(self,elem: u32) -> List {
+        Cons(elem, Box::new(self))
+    } 
+
+    //返回链表的长度
+    fn len(&self) -> u32 {
+        //*self = List , self = &List
+        match *self {
+            //ref 对tail的引用
+            Cons(_, ref tail) => 1 + tail.len(),
+            Nil => 0,
+        }
+    }
+
+    //返回链表的字符串表示
+    fn stringify(&self) -> String {
+        match *self {
+            Nil => {
+                format!("Nil")
+            },
+            Cons(head, ref tail) => {
+                //format不会打印到控制台上，而是返回一个堆分配的字符串
+                format!("{} {}", head, tail.stringify())
+            },
+        }
+    }
+}
+
+fn use_list() {
+    let mut list = List::new();
+
+    list = list.prepend(1);
+    list = list.prepend(2);
+    list = list.prepend(3);
+
+    println!("linked list has length: {}", list.len());
+    println!("list string: {}", list.stringify());
+}
+
+
+//声明全局常量
+const THRESHOLD: i32 = 10;
+//类型标记 &'static str 
+static LANGUAGE: &'static str = "Rust";
+
+fn is_big( n: i32) -> bool {
+    n > THRESHOLD
+}
+
+fn use_const() {
+    let n = 19;
+
+    println!("This is {}", LANGUAGE);
+    println!("The threshold is {}", THRESHOLD);
+    println!("{} is {}", n, if is_big(n) { "big" } else {"small"} );
+
+    
+}
