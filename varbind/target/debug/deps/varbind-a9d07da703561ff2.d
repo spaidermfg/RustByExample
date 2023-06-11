cf348306474bff0a597469940aa4bbb19978b005
Toymachine <488434442@qq.com>
2023-06-11 23:10:18 +0800

    test

---
+++ varbind/target/debug/deps/varbind-a9d07da703561ff2.d
@@ -0,0 +1,5 @@
+/Users/mac/myth/rustaceans/RustByExample/varbind/target/debug/deps/varbind-a9d07da703561ff2: src/main.rs
+
+/Users/mac/myth/rustaceans/RustByExample/varbind/target/debug/deps/varbind-a9d07da703561ff2.d: src/main.rs
+
+src/main.rs:
