93ce7ac7f82d72d82780a6abb6da78c6794dae45
Toymachine <488434442@qq.com>
2023-06-20 22:51:19 +0800

    fix:类型系统

---
+++ varbind/target/debug/deps/varbind-366e7b04efc160e5.d
@@ -0,0 +1,5 @@
+/Users/mac/myth/rustaceans/RustByExample/varbind/target/debug/deps/varbind-366e7b04efc160e5.rmeta: src/main.rs
+
+/Users/mac/myth/rustaceans/RustByExample/varbind/target/debug/deps/varbind-366e7b04efc160e5.d: src/main.rs
+
+src/main.rs:
---
+++ varbind/target/debug/deps/varbind-475f32e780a04db5.d
@@ -0,0 +1,5 @@
+/Users/mac/myth/rustaceans/RustByExample/varbind/target/debug/deps/varbind-475f32e780a04db5.rmeta: src/main.rs
+
+/Users/mac/myth/rustaceans/RustByExample/varbind/target/debug/deps/varbind-475f32e780a04db5.d: src/main.rs
+
+src/main.rs:
