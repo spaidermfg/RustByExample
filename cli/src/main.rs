6620bcac70c0af1e70c25b03e48565a42043a761
Toymachine <488434442@qq.com>
2023-07-17 17:31:55 +0800

    cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,28 +1,67 @@
 /// clap test
 fn main() {
-    println!("Hello, world!");
-
     command();
 }
 
+use std::path::PathBuf;
 
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
-
-#[derive(Parser, Debug)]
+#[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long)]
-    name: String,
+    name: Option<String>,
 
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+    #[arg(short, long, value_name="FILE")]
+    config: Option<PathBuf>,
+
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+
+#[derive(Subcommand)]
+enum Commands {
+    Test {
+        #[arg(short, long)]
+        list: bool,
+    },  
 }
 
 fn command() {
     let args = Args::parse;
 
-    for _ in 0..args().count {
-        println!("Hello {}: ", args().name);
-    }  
+    //name
+    if let Some(name) = args().name.as_deref() {
+        println!("Value for name: {}", name);
+    }
+
+    //config
+    if let Some(config_path) = args().config.as_deref() {
+        println!("Value for config: {}", config_path.display());
+    }
+
+    //debug
+    match args().debug {
+        0 => println!("Debug mode is off"),
+        1 => println!("Debug mode is kind of on"),
+        2 => println!("Debug mode is on"),
+        _ => println!("Don't be crazy"),
+    }
+
+    //command
+    match &args().command {
+        Some(Commands::Test { list }) => {
+            if *list {
+                println!("Printing testing lists...");
+            } else {
+                println!("Not printing testing lists...");
+            }
+        }
+        None => {}
+    }
+
 }
