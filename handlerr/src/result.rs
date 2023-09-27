ddb463d38cd738ea5761507e4a89518203f140ab
Toymachine <488434442@qq.com>
2023-09-27 22:20:45 +0800

    Result<T,E> alias

--- handlerr/src/result.rs
+++ handlerr/src/result.rs
@@ -2,6 +2,9 @@ use std::num::ParseIntError;
 
 /// Result 描述的是可能是可能的错误而不是可能的不存在
 /// OK<T>  Err<E>
+/// 如果某个Result可能被重用，可以为其取一个别名
+
+type AliiasedResult<T> = Result<T, ParseIntError>;
 
 pub fn r#use() {
     println!("---------------------------result");
@@ -19,7 +22,7 @@ pub fn r#use() {
     print(t);
 }
 
-fn multiply_v1(first_num_str: &str, second_num_str: &str) -> Result<i32, ParseIntError> {
+fn multiply_v1(first_num_str: &str, second_num_str: &str) -> AliiasedResult<i32> {
     match first_num_str.parse::<i32>() {
         Err(e) => Err(e),
         Ok(first_num) => {
@@ -33,15 +36,14 @@ fn multiply_v1(first_num_str: &str, second_num_str: &str) -> Result<i32, ParseIn
     }
 }
 
-fn multiply_v2(first_num_str: &str, second_num_str: &str) -> Result<i32, ParseIntError> {
+fn multiply_v2(first_num_str: &str, second_num_str: &str) -> AliiasedResult<i32> {
      first_num_str.parse::<i32>().and_then(|first_num| {
          second_num_str.parse::<i32>().map(|second_str|
          first_num * second_str)
      })
 }
 
-
-fn print(result: Result<i32, ParseIntError>) {
+fn print(result: AliiasedResult<i32>) {
     match result {
         Ok(n) => println!("n is {}", n),
         Err(e) => println!("Error: {}", e),
