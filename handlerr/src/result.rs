d25c54f4aceb9be9e99ca6fefae4c0113e6d8c44
Toymachine <488434442@qq.com>
2023-09-28 21:24:39 +0800

    early return

--- handlerr/src/result.rs
+++ handlerr/src/result.rs
@@ -1,6 +1,6 @@
 use std::num::ParseIntError;
 
-/// Result 描述的是可能是可能的错误而不是可能的不存在
+/// Result 描述的是可能的错误而不是可能的不存在
 /// OK<T>  Err<E>
 /// 如果某个Result可能被重用，可以为其取一个别名
 
@@ -24,10 +24,10 @@ pub fn r#use() {
 
 fn multiply_v1(first_num_str: &str, second_num_str: &str) -> AliasedResult<i32> {
     match first_num_str.parse::<i32>() {
-        Err(e) => Err(e),
+        Err(e) => return Err(e),
         Ok(first_num) => {
             match second_num_str.parse::<i32>() {
-                Err(e) => Err(e),
+                Err(e) => return Err(e),
                 Ok(second_num) => {
                     Ok(first_num * second_num)
                 }
---
+++ handlerr/target/debug/deps/handlerr-1f9e8a80dd56a162.d
@@ -0,0 +1,7 @@
+/Users/mac/myth/rustaceans/RustByExample/handlerr/target/debug/deps/handlerr-1f9e8a80dd56a162: src/main.rs src/who_people.rs src/result.rs
+
+/Users/mac/myth/rustaceans/RustByExample/handlerr/target/debug/deps/handlerr-1f9e8a80dd56a162.d: src/main.rs src/who_people.rs src/result.rs
+
+src/main.rs:
+src/who_people.rs:
+src/result.rs:
